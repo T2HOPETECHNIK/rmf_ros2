a0b5df688bca8e585606e4bba5675f36695d09a1
JeremiahLim-HT <jeremiah.lim@hopetechnik.com>
2026-01-10 16:04:02 +0800

    fixed indentation

--- read_only_mutex_supervisor/read_only_mutex_supervisor/read_only_mutex_supervisor/read_only_mutex_supervisor.py
+++ read_only_mutex_supervisor/read_only_mutex_supervisor/read_only_mutex_supervisor/read_only_mutex_supervisor.py
@@ -118,7 +118,7 @@ class ReadOnlyMutexSupervisor(Node):
     def fleet_state_callback(self, msg):
         """Handle the fleet state and manage mutexes for robots."""
         if not self.fleet_name or msg.name not in self.fleet_name:
-          return
+            return
         for robot in msg.robots:
             robot_in_mutex = False
             x = robot.location.x
