67185ceedb10925c0b3d0846cedcfd15c8d5503a
JeremiahLim-HT <jeremiah.lim@hopetechnik.com>
2026-01-15 17:27:21 +0800

    Update ROS2 with manual cherry pick for mutex group

--- rmf_fleet_adapter/src/rmf_fleet_adapter/events/GoToPlace.cpp
+++ rmf_fleet_adapter/src/rmf_fleet_adapter/events/GoToPlace.cpp
@@ -730,9 +730,9 @@ void GoToPlace::Active::_execute_plan(
 
     const auto& graph = _context->navigation_graph();
     _context->retain_mutex_groups(
-      {graph.get_waypoint(goal.waypoint()).in_mutex_group()});
-    if (_is_final_destination)
-      _finished();
+      {graph.get_waypoint(goal.waypoint()).in_mutex_group()}, "empty plan");
+    
+    _finished();
     return;
   }
 
